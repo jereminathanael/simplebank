475e3addf542d02d4a839be820f08f2b857e9e62
jereminathanael <jereminathanael332@gmail.com>
2026-03-04 12:37:40 +0700

    send async tasks to redis within a DB transaction

--- db/sqlc/tx_create_user.go
+++ db/sqlc/tx_create_user.go
@@ -9,15 +9,22 @@ type CreateUserTxParams struct {
 }
 
 type CreateUserTxResult struct {
+	User User
 }
 
 // CreateUserTx performs a money transfer from one account to the other.
 func (store *SQLStore) CreateUserTx(ctx context.Context, arg CreateUserTxParams) (CreateUserTxResult, error) {
 	var result CreateUserTxResult
+
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
 
-		return err
+		result.User, err = q.CreateUser(ctx, arg.CreateUserParams)
+		if err != nil {
+			return err
+		}
+
+		return arg.AfterCreate(result.User)
 	})
 
 	return result, err
